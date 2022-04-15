0cc1c835116f7695a5cb407a035ca49767212022
ramaddict <apbiancofriber@gmail.com>
2022-04-15 00:44:14 -0300

    Initial commit Initialize repository with springboot

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Desafio BRY
+    
+### Dependencies
+    You will need docker & docker-compose
+
+### Running
+    docker-compose up
---
+++ src/main/java/br/com/bry/bianco/desafio/DesafioApplication.java
@@ -0,0 +1,16 @@
+package br.com.bry.bianco.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@SpringBootApplication
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+		log.info("pix");
+	}
+
+}
