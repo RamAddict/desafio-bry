112bdd1941e069e8dc4526a51a3cab4e7dcac248
ramaddict <apbiancofriber@gmail.com>
2022-04-19 17:19:14 -0300

    add basic exception treatment

--- src/main/java/br/com/bry/bianco/desafio/RootController.java
+++ src/main/java/br/com/bry/bianco/desafio/RootController.java
@@ -1,39 +1,63 @@
 package br.com.bry.bianco.desafio;
 
-import java.io.IOException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateEncodingException;
-import java.security.cert.CertificateException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.bouncycastle.cms.CMSException;
-import org.bouncycastle.operator.OperatorCreationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @RestController
 public class RootController {
+	@SuppressWarnings("rawtypes")
 	@RequestMapping(path = "/signature", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public String requestDocSignature(@RequestParam("toSign") MultipartFile toSign,
+	public ResponseEntity requestDocSignature(@RequestParam("toSign") MultipartFile toSign,
 			@RequestParam("pkcsFile") MultipartFile pkcsFile,
-			@RequestParam("password") String password)
-			throws UnrecoverableKeyException, CertificateEncodingException, KeyStoreException, NoSuchAlgorithmException,
-			CertificateException, OperatorCreationException, IOException, CMSException {
+			@RequestParam("password") String password) throws JsonProcessingException {
 
-		return DesafioApplication.signDoc(toSign.getInputStream(), pkcsFile.getInputStream(), password.toCharArray(),
-				"", "f22c0321-1a9a-4877-9295-73092bb9aa94");
+		try {
+			var res = DesafioApplication.signDoc(toSign.getInputStream(), pkcsFile.getInputStream(),
+					password.toCharArray(),
+					"", "f22c0321-1a9a-4877-9295-73092bb9aa94");
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(res);
+		} catch (Exception e) {
+			log.error(e);
+			var mapper = new ObjectMapper();
+			var errorResponse = mapper.createObjectNode();
+			errorResponse.put("error", e.getMessage());
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(errorResponse));
+		}
 	}
 
+	@SuppressWarnings("rawtypes")
 	@RequestMapping(path = "/verify", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public String verifySignature(@RequestParam("signedMessage") MultipartFile signedMessage)
-			throws UnrecoverableKeyException, CertificateEncodingException, KeyStoreException, NoSuchAlgorithmException,
-			CertificateException, OperatorCreationException, IOException, CMSException {
-
-		return DesafioApplication.verifyDoc(signedMessage.getInputStream()) ? "VALIDO" : "INVALIDO";
+	public ResponseEntity verifySignature(@RequestParam("signedMessage") MultipartFile signedMessage)
+			throws JsonProcessingException {
+		try {
+			var res = DesafioApplication.verifyDoc(signedMessage.getInputStream()) ? "VALIDO" : "INVALIDO";
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(res);
+		} catch (Exception e) {
+			log.error(e);
+			var mapper = new ObjectMapper();
+			var errorResponse = mapper.createObjectNode();
+			errorResponse.put("error", e.getMessage());
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(errorResponse));
+		}
 	}
 }
